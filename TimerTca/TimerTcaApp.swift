b2fbcb2f6ddbdd6598f4b999a19bf1e2389443ee
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2024-07-30 19:56:53 -0400

    Initial Commit

---
+++ TimerTca/TimerTcaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerTcaApp.swift
+//  TimerTca
+//
+//  Created by Gaurav Bhambhani on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerTcaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
